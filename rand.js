66bb4bc3e0d42981da753ac38f2a0a78405b7a29
ashkhan1999 <ashotkhan@gmail.com>
2017-08-01 19:57:01 +0400

    .,

--- rand.js
+++ rand.js
@@ -1,12 +1,12 @@
 function Rand(arr) {
-	this.arr = arr,
-	this.random = function(ar) {
-		ar = this.arr;
-		var pos = Math.floor((Math.random() * arr.length) + 0);
-		return ar[pos];
-	}
+	this.arr = arr
 }
 
-var array = new Rand([1, 2, 456, 878, 45, 56, 78, 89, 83, 5, 9, 999, 511, 664, 0])
+Rand.prototype.random = function() {
+		var pos = Math.floor((Math.random() * this.arr.length) + 0);
+		console.log(this.arr[pos]);
+}
+	
+var array = new Rand([1, 2, 456, 878, 45, 56, 78, 89, 83, 5, 9, 999, 511, 664, 0]);
 
-console.log(array.random())
+array.random()
